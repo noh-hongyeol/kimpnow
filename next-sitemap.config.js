5e765ff2bba10bbced7988ec2571729d06f5359a
noh-hongyeol <e18901@gmail.com>
2025-05-16 21:44:32 +0900

    fix: override auto-generated robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://kimpnow.com',
-    generateRobotsTxt: true,
+    
     sitemapSize: 5000,
     outDir: './public',      // ✅ 이 줄 중요!
   };
